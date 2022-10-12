3f243ba97395a9e7736565468561952341d96e5b
prpercy <prpercy@gmail.com>
2022-10-11 23:46:25 -0400

    =added user interface functionalities

--- user/user.py
+++ user/user.py
@@ -5,6 +5,9 @@ import sqlalchemy as sql
 import pandas as pd
 import questionary as qs
 from questionary import Validator, ValidationError, prompt
+from portfolio.portfolio import perform_portfolio_analysis
+from stock.stock import perform_stock_analysis
+from stock.trade import perform_trade_stock
 
 #user5
 # class definition to validate user name
@@ -20,6 +23,55 @@ class NameValidator(Validator):
                 message="Please enter a value that has length shorter than 8",
                 cursor_position=len(document.text),
             )
+
+
+
+# function to load user options
+def load_user_options():
+    signed_in_user_choices = ['Update available amount for trading', 'Stock Analysis','Portfolio Analysis', 'Trade Stocks' , 'Delete User']
+    user_choice = qs.select(
+        "What would you like to do?",
+        choices=signed_in_user_choices
+    ).ask()
+       
+    return user_choice
+
+
+# update user fund i.e. amount available to trade for user
+def update_user_fund(user_df):
+    user_funds = qs.text(
+        "Please enter the amount you would like to trade"
+    ).ask()
+    
+    # update user details into the database in user table
+    print('updated the funds into database')
+    
+    return True
+
+# function to load user options
+def execute_user_choice(user_df, user_choice):
+    if user_choice == 'Update available amount for trading':
+        update_user_fund(user_df)
+    elif user_choice == 'Stock Analysis':
+        print('perform stock analysis')
+        user_stock = qs.text(
+            "Please enter the stock you would like to analyse"
+        ).ask()
+        perform_stock_analysis(user_stock,user_df)
+    elif user_choice == 'Portfolio Analysis':
+        print('perform portfolio analysis...')
+        perform_portfolio_analysis(user_df)
+    elif user_choice == 'Trade Stocks':
+        trade_stock_choices = ['Buy', 'Sell']
+        user_trade_choice = qs.select(
+            "What would you like to do?",
+            choices=trade_stock_choices
+        ).ask()
+        perform_trade_stock(user_trade_choice,user_df)
+    else:
+        delete_user(user_df)
+       
+    return True
 
 # function to request user to input user name and password
 def request_user_credentials():
@@ -129,10 +181,8 @@ def sign_in_user(user_df, db_engine):
         print('Incorrect user name. Please enter correct user name or sign up.')
     return user_db_df
 
-# function to update user details into the database in user table
-def update_user_fund(user_df):
-    return True
 
 # function to delete user from the database (from user table)
 def delete_user(user_df):
+    print('user is deleted')
     return True
