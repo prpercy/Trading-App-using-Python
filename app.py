f806b2aa01fc72a6e847241115eb90400b7099d9
prpercy <prpercy@gmail.com>
2022-10-11 23:45:15 -0400

    =added user authentication and user choices in app

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import fire
 import questionary as qs
 import sqlalchemy as sql
-from user.user import load_authentication
+from user.user import load_authentication, load_user_options, execute_user_choice
 
-def request_user_service():
-    
+
 
         
             
@@ -15,10 +14,17 @@ def run():
     user_df = load_authentication()
     
     print(user_df.head())
+    
+    # present user choices after successful login
+    user_choice = load_user_options()
+    print(user_choice)
+    
+    execute_user_choice(user_df, user_choice)
+    
     # Ask what would user like to do next, namely, 
     #  (1) trade (2) update user available fund to trade (3) delete the user (4) analyse the portfolio if exists (5) generate reports
     
-    request_user_service()
+    #request_user_service()
    
     
 
