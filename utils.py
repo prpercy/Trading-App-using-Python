d4564e380d07530b9f4b79de816f67de05aeb71c
prpercy <prpercy@gmail.com>
2022-10-14 01:32:47 -0400

    =moved some common functions into utils

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from finta import TA
 import pandas as pd
 from questionary import Validator, ValidationError, prompt
 import matplotlib.pyplot as plt
+import sqlalchemy as sql
 
 plt.style.use('fivethirtyeight')
 
@@ -31,3 +32,37 @@ class NameValidator(Validator):
                 message="Please enter a value that has length shorter than 8",
                 cursor_position=len(document.text),
             )
+
+# create database engine
+def get_db_engine():
+    # Create a database connection string
+    db_connection_string = 'sqlite:///./resources/app.db'
+    
+    # Create a database engine
+    db_engine = sql.create_engine(db_connection_string)
+    
+    return db_engine
+
+# initiate database tables
+def initiate_database_tables(db_engine):
+    create_user_table = """
+           CREATE TABLE user (
+            user_name VAR PRIMARY KEY,
+            user_password VAR,
+            user_available_to_trade DOUBLE
+        )
+        """
+    db_engine.execute(create_user_table)
+        
+    create_portfolio_table = """
+        CREATE TABLE portfolio (
+            id INT IDENTITY(1,1) PRIMARY KEY,
+            ticker VAR,
+            number_of_shares DOUBLE,
+            user_name VAR,
+            FOREIGN KEY (user_name) REFERENCES user(user_name)
+        )
+        """
+    db_engine.execute(create_portfolio_table)
+        
+    return True
